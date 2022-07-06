fe8bed535d04e2b49b2607407a8054692923343e
Fakhruz Bakhromov <kirademiurge@yandex.com>
2022-07-06 19:07:05 +0500

    redesigned ui of TranslatePage component

--- src/pages/TranslatePage.jsx
+++ src/pages/TranslatePage.jsx
@@ -1,10 +1,36 @@
+import { useState, useContext } from "react"
+import { SecretKeyCnx } from "../context/SecretKeyCnx";
+
 export const TranslatePage = () => {
+
+	const [plainText, setPlainText] = useState("");
+	const [encryptedMsg, setEncryptedMsg] = useState("");
+	const [secretKey, setSecretKey] = useContext(SecretKeyCnx);
+
+	const onChangePlainText = (event) => {
+		setPlainText(event.target.value);
+	}
+	console.log(plainText);
 	
 	return (
-		<div className="container mx-auto px-12 flex w-full">
-			<div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">type anything...</div>
-			<div className="divider divider-horizontal select-none">OR</div>
-			<div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">result</div>
+		<div className="space-y-4">
+			<div className="container mx-auto px-12 flex w-full space-x-6">
+				<h3 className="w-full text-lg">Plain text</h3>
+				<h3 className="w-full text-lg">Encrypted message</h3>
+			</div>
+			<div className="container mx-auto px-12 flex w-full space-x-6">
+
+				<textarea
+					placeholder="type a message..."
+					defaultValue={plainText}
+					onChange={ onChangePlainText }
+					className="textarea textarea-ghost resize-none w-full h-60 bg-base-300" />
+
+				<textarea
+					defaultValue={encryptedMsg}
+					className="textarea textarea-ghost resize-none w-full h-60 bg-base-300" />
+
+			</div>
 		</div>
 	)
 }
